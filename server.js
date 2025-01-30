c573d2724519d6b51895c2f5642e5325c8e0af26
AndreyGOIT <ae.borga@gmail.com>
2025-01-30 13:51:14 +0200

    add fetch on main page

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const employees = require('./data/employees.json');
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//--staattinen web palvelin-------- 
+const path = require('path');
+const htmlPath = path.join(path.join(__dirname, '/public'));
+app.use(express.static(htmlPath));
+
+//--REST API palvelin-----
+app.get('/api/employees', (req, res) => {
+    res.send(employees);
 });
 
+//palvelimen käynnistys
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);    
